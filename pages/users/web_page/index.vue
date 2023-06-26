dcae130b4a27e8912dab4d3cc98927db677dd8b9
YunaiV <zhijiantianya@gmail.com>
2023-06-27 00:36:49 +0800

    【WebView 界面】 1. 整理代码

--- pages/users/web_page/index.vue
+++ pages/users/web_page/index.vue
@@ -1,13 +1,8 @@
 <template>
-	<web-view class="web-view" :webview-styles="webviewStyles" :src="url" :style="{width: windowW + 'px', height: windowH + 'px'}"></web-view>
+	<web-view class="web-view" :webview-styles="webviewStyles" :src="url" :style="{width: windowW + 'px', height: windowH + 'px'}" />
 </template>
-
 <script>
-	import {
-		mapGetters
-	} from "vuex";
 	export default {
-		//computed: mapGetters(['chatUrl']),
 		data() {
 			return {
 				windowH: 0,
@@ -21,7 +16,9 @@
 			}
 		},
 		onLoad(option) {
-			if(option.webUel) this.url = option.webUel;
+			if (option.webUel) {
+        this.url = option.webUel;
+      }
 			uni.setNavigationBarTitle({
 				title: option.title
 			})
