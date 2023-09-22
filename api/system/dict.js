a91b09a5fc131ca06791767c5b6359b7b6c5d486
owen <owen@evolsun.com>
2023-09-22 15:11:03 +0800

    system: uniapp增加字典数据查询接口

---
+++ api/system/dict.js
@@ -0,0 +1,6 @@
+import request from "@/utils/request.js";
+
+// 根据字典类型查询字典数据信息
+export function getDicts(dictType) {
+  return request.get('app-api/system/dict-data/type/' + dictType);
+}
---
+++ utils/dict.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 芋道源码
+ *
+ * 数据字典工具类
+ */
+
+export const DICT_TYPE = {
+
+  // ========== MALL - 交易模块 ==========
+  BROKERAGE_BANK_NAME: 'brokerage_bank_name', // 佣金提现银行
+
+}
