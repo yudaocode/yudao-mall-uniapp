03ceb0bcf3d2491a2f61748b062dc9efd6c8a222
YunaiV <zhijiantianya@gmail.com>
2023-09-30 18:54:26 +0800

    wallet：接入钱包充值

--- api/pay/wallet.js
+++ api/pay/wallet.js
@@ -9,3 +9,13 @@ export function getPayWallet() {
 export function getWalletTransactionPage(data) {
   return request.get("app-api/pay/wallet-transaction/page", data);
 }
+
+// 获得钱包充值套餐列表
+export function getWalletRechargePackageList() {
+  return request.get("app-api/pay/wallet-recharge-package/list");
+}
+
+// 创建钱包充值记录（发起充值）
+export function createWalletRecharge(data) {
+  return request.post("app-api/pay/wallet-recharge/create", data);
+}
