245a04aedc02074a0c83d10b8acfbc61b6fdaecc
YunaiV <zhijiantianya@gmail.com>
2023-11-20 23:45:31 +0800

    修复获得商品的活动信息时，数组参数在小程序兼容性存在问题

--- api/promotion/activity.js
+++ api/promotion/activity.js
@@ -12,7 +12,7 @@ export function getActivityListBySpuId(spuId) {
 // 获得多个商品，近期参与的每个活动
 export function getActivityListBySpuIds(spuIds) {
   return request.get("app-api/promotion/activity/list-by-spu-ids", {
-    spuIds
+    spuIds: spuIds && spuIds.length > 0 ? spuIds.join(',') : undefined
   }, {
     noAuth: true // TODO 芋艿：后续要做调整
   });
