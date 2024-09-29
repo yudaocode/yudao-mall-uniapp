09dc98db16e043296de40e34a84563ee75b19e3d
YunaiV <zhijiantianya@gmail.com>
2024-09-29 09:43:29 +0800

    【代码评审】积分商城部分

--- pages/activity/point/list.vue
+++ pages/activity/point/list.vue
@@ -1,4 +1,4 @@
-<!-- 页面  -->
+<!-- 积分商城：商品列表  -->
 <template>
   <s-layout title="积分商城">
     <view class="ss-p-20">
@@ -8,14 +8,14 @@
           :data="item"
           priceColor="#FF3000"
           @tap="sheep.$router.go('/pages/goods/point', { id: item.id })"
-        ></s-point-card>
+        />
       </view>
     </view>
     <s-empty
       v-if="state.pagination.total === 0"
       icon="/static/goods-empty.png"
       text="暂无积分商品"
-    ></s-empty>
+    />
     <uni-load-more
       v-if="state.pagination.total > 0"
       :status="state.loadStatus"
@@ -42,6 +42,7 @@
     loadStatus: '',
   });
   async function getData(page = 1, list_rows = 5) {
+    // TODO @puhui999：分页重写成，我们的代码风格。原先的有点复杂
     state.loadStatus = 'loading';
     let res = await sheep.$api.app.scoreShop.list({
       list_rows,
